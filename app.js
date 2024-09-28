01919e1ec1ea3bccb940d0508fcc5457ec397ab7
tomek-m-coder <60541005+tomek-m-coder@users.noreply.github.com>
2024-09-28 22:42:05 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-let vm = Vue.createApp()
-methods: {
+Vue.createApp({
+   data(){
+    return{
+        perspective: 100,
+        rotateX: 0,
+        rotateY: 0,
+        rotateZ: 0
+
+    }
     
-}
+   },
+   computed:{
+    box(){
+        return{
+            transform: `perspective(${this.perspective}px)
+            rotateX(${this.rotateX}deg)
+            rotateY(${this.rotateY}deg)
+            rotateZ(${this.rotateZ}deg)`
+        }
+    }
+   }
+}).mount('#app')
